fef72b20119631e7c4b2b0f85516383fd1034c96
rafaelcbarbosa <50080617+rafaelcbarbosa@users.noreply.github.com>
2020-03-01 15:46:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', function(event) {
             ]); 
         }) 
     ); 
-}); 
+}
   
 if (!('fetch' in window)) { 
     console.log('Fetch API not found, try including the polyfill'); 
