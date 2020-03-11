c57475001a2f7ea699ca8afea13ee0b236165804
rafaelcbarbosa <50080617+rafaelcbarbosa@users.noreply.github.com>
2020-03-10 23:02:52 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,10 +52,4 @@ if ('serviceWorker' in navigator) {
             console.log('CLIENT: ServiceWorker registration failed: ', err); 
         }); 
     }); 
-} 
-
-var timeout = setInterval(reCheck, 3000);
-
-function reCheck () {
-  $('#gate_status').load('http://coxinha.asuscomm.com:9080/getgatestatus');
 }
