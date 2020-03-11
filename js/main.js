0b185eb2b79d1937693e20ac09ea6808c2739420
rafaelcbarbosa <50080617+rafaelcbarbosa@users.noreply.github.com>
2020-03-10 22:57:14 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,5 +57,5 @@ if ('serviceWorker' in navigator) {
 var timeout = setInterval(reCheck, 3000);
 
 function reCheck () {
-  $('#gate_status').load('https://coxinha.asuscomm.com:9443/gate_status');
+  $('#gate_status').load('http://coxinha.asuscomm.com:9080/getgatestatus');
 }
