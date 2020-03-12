f09b9428d19d22493478b09c7df23b115c2b4658
rafaelcbarbosa <50080617+rafaelcbarbosa@users.noreply.github.com>
2020-03-12 20:23:23 -0300

    Update gate_status.js

--- js/gate_status.js
+++ js/gate_status.js
@@ -1,6 +1,5 @@
 var timeout = setInterval(reCheck, 3000);
 
 function reCheck () {
-//  $('#gate_status').load('http://coxinha.asuscomm.com:9080/getgatestatus');
-    $('#gate_status').get('http://coxinha.asuscomm.com:9080/getgatestatus');
+  $('#gate_status').load('http://coxinha.asuscomm.com:9080/getgatestatus');
 }
